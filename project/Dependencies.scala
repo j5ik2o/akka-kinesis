efb9ee3330ba8f1f77ac92aa8868e6d4d21c68bc
Scala Steward <me@scala-steward.org>
2021-04-15 22:26:56 +0200

    Update aws-java-sdk-cloudwatch, ... to 1.11.1000

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val scala212Version            = "2.12.13"
     val scala213Version            = "2.13.5"
-    val awsSdkVersion              = "1.11.999"
+    val awsSdkVersion              = "1.11.1000"
     val akkaVersion                = "2.6.13"
     val testcontainersScalaVersion = "0.39.3"
     val scalaTestVersion           = "3.2.6"
